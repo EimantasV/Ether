180affd9482610e36d6c8df95cab542f63338ae7
EimantasVinciunas <eimantas.vinciunas@mif.stud.vu.lt>
2021-12-14 22:20:36 +0200

    done?

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import React, { Component, useState } from "react";
+import TikTacToeContract from "./contracts/TicTacToe.json";
 import getWeb3 from "./getWeb3";
 
 import "./App.css";
 
-class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+function App() {
+  const [state, updateState] = useState({ web3: null, accounts: null, contract: null });
+  const [players, updatePs] = useState({ P1: false, P2: false });
 
-  componentDidMount = async () => {
+  async function loadWeb3() {
     try {
       // Get network provider and web3 instance.
       const web3 = await getWeb3();
@@ -17,57 +18,112 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
+      const deployedNetwork = TikTacToeContract.networks[networkId];
+      const instance = new web3.eth.Contract(TikTacToeContract.abi, deployedNetwork && deployedNetwork.address);
 
       // Set web3, accounts, and contract to the state, and then proceed with an
       // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      updateState({ web3, accounts, contract: instance });
+      const p1 = await instance.methods.player1().call();
+      const p2 = await instance.methods.player2().call();
+      updatePs({ P1: p1 === accounts[0], P2: p2 === accounts[0] });
     } catch (error) {
       // Catch any errors for any of the above operations.
-      alert(
-        `Failed to load web3, accounts, or contract. Check console for details.`,
-      );
+      alert(`Failed to load web3, accounts, or contract. Check console for details.`);
       console.error(error);
     }
-  };
+  }
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
+  async function run() {
     // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
 
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
-  };
-
-  render() {
-    if (!this.state.web3) {
-      return <div>Loading Web3, accounts, and contract...</div>;
+    const response = await state.contract.methods.getBoard().call();
+    console.log(response);
+    for (let i = 0; i < 9; i++) document.getElementById(String(i)).innerHTML = response[i] != "0" ? (response[i] === "1" ? "X" : "O") : "";
+  }
+  async function escape() {
+    try {
+      await state.contract.methods.escape().send({ from: state.accounts[0] });
+      const p1 = await state.contract.methods.player1().call();
+      const p2 = await state.contract.methods.player2().call();
+      updatePs({ P1: p1 === state.accounts[0], P2: p2 === state.accounts[0] });
+    } catch {
+      console.log("failed");
     }
+  }
+  async function PlayAsX() {
+    try {
+      await state.contract.methods.startPlayingAsX().send({ from: state.accounts[0], value: 1000000000000000000 });
+      const response = await state.contract.methods.player1().call();
+      if (response > 0) {
+        updatePs({ P1: true, P2: players.P2 });
+      }
+    } catch {
+      console.log("failed");
+    }
+  }
+  async function PlayAsO() {
+    try {
+      await state.contract.methods.startPlayingAsO().send({ from: state.accounts[0], value: 1000000000000000000 });
+      const response = await state.contract.methods.player2().call();
+      if (response > 0) {
+        updatePs({ P1: players.P1, P2: true });
+      }
+    } catch {
+      console.log("failed");
+    }
+  }
+
+  async function move(id) {
+    try {
+      await state.contract.methods.move(id).send({ from: state.accounts[0] });
+    } catch {
+      console.log("failed");
+    }
+  }
+  if (!state.web3)
     return (
-      <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+      <div className="menuAlign">
+        <button className="connect" onClick={() => loadWeb3()}>
+          Load web3
+        </button>
       </div>
     );
-  }
+  setInterval(run, 5000);
+  console.log(state.accounts);
+
+  return (
+    <div className="fade">
+      <div className="navigation">Ultimate Tic Tac Toe</div>
+
+      <div className="alignment">
+        <div style={{ display: "grid" }}>
+          <button className={"play" + (players.P1 ? " green" : "")} onClick={() => PlayAsX()}>
+            Play as X
+          </button>
+          <button className="play" onClick={() => escape()}>
+            Escape
+          </button>
+        </div>
+        <div className="grid">
+          <div className="element" id="0" onClick={() => move(0)}></div>
+          <div className="element" id="1" onClick={() => move(1)}></div>
+          <div className="element" id="2" onClick={() => move(2)}></div>
+          <div className="element" id="3" onClick={() => move(3)}></div>
+          <div className="element" id="4" onClick={() => move(4)}></div>
+          <div className="element" id="5" onClick={() => move(5)}></div>
+          <div className="element" id="6" onClick={() => move(6)}></div>
+          <div className="element" id="7" onClick={() => move(7)}></div>
+          <div className="element" id="8" onClick={() => move(8)}></div>
+        </div>
+        <div>
+          <button className={"play" + (players.P2 ? " green" : "")} onClick={() => PlayAsO()}>
+            Play as O
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
--- contracts/TicTacToe.sol
+++ contracts/TicTacToe.sol
@@ -1,20 +1,20 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7 <0.9.0;
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity >=0.7.0 <0.9.0;
 
 contract TicTacToe {
 
     event Win(string _player, address _address, uint256 _jackpot);
     event Tie(string _msg, address _address1, address _address2, uint256 _jackpot);
 
-    address payable public player1;
-    address payable public player2;
+    address payable public player1; // X
+    address payable public player2; // O
 
     uint256[9] board; // 0 - top left, order is like reading a book.
     bool turn; // true - O, false - X
 
-    uint256 playerCount=0;
+    uint256 public blockNumOld = block.number;
 
-    function startPlyingAsX() external payable {
+    function startPlayingAsX() external payable {
 
         require(player2 != msg.sender, "can't play as both");
         require(player1 == address(0), "position taken");
@@ -22,8 +22,7 @@ contract TicTacToe {
         player1 = payable( msg.sender);
 
     }
-    function startPlyingAsO() external payable {
-
+    function startPlayingAsO() external payable {
         require(player1 != msg.sender, "can't play as both");
         require(player2 == address(0), "position taken");
         require(msg.value >= 1 ether, "bet must be >=1 ether");
@@ -46,12 +45,11 @@ contract TicTacToe {
         require(board[id] == 0, "position taken");
 
         board[id] = turn?2:1; // player2 - 2, player1 - 1;
-
+        blockNumOld = block.number;
         checkState();
     }
     function checkState() public payable
     {
-        
         // check for winner
         uint256 t = turn?2:1;
         turn = ! turn;
@@ -102,6 +100,15 @@ contract TicTacToe {
                 emit Win("Player 2 (O) Won",player2,address(this).balance);
                 player2.transfer(address(this).balance);
             }
+
+            player1 = payable(address(0));
+            player2 = payable(address(0));
+            turn = false;
+            for(uint256 i = 0;i<9;i++)
+            {
+                board[i]=0;
+            }
+
         }
 
         if(winner==false)
@@ -122,15 +129,61 @@ contract TicTacToe {
                 emit Tie("Game is Tie", player1,player2,address(this).balance);
                 player1.transfer(address(this).balance/2);
                 player2.transfer(address(this).balance);
+                
+                    player1 = payable(address(0));
+                    player2 = payable(address(0));
+                    turn = false;
+                    for(uint256 i = 0;i<9;i++)
+                    {
+                        board[i]=0;
+                    }
             }
         }
 
+    }
+    function escape() public payable
+    {
+        bool escaped = false;
+        if(msg.sender == player1 && player2 == address(0)) // changed mind
+        {
+            require(player2==address(0),"game started");
+            player1.transfer(address(this).balance);
+            escaped = true;
+        }
+        else if(msg.sender == player2 && player1 == address(0)) // changed mind
+        {
+            require(player1==address(0),"game started");
+            player2.transfer(address(this).balance);
+            escaped = true;
+        }
+        else if (turn) // player2 afk
+        {
+            require(blockNumOld+20 < block.number, "too early"); // ~5min afk
+            player1.transfer(address(this).balance);
+            escaped = true;
+        }
+        else // player1 afk
+        {
+            require(blockNumOld+20 < block.number, "too early");
+            player2.transfer(address(this).balance);
+            escaped = true;
+        }
+
+        if(escaped) // reset game
+        {
+            player1 = payable(address(0));
+            player2 = payable(address(0));
+            turn = false;
+            for(uint256 i = 0;i<9;i++)
+            {
+                board[i]=0;
+            }
+        }
     }
 
     function getBoard() external view returns (uint256 [9] memory){
         return board;
     }
-
     function checkJackpot() external view returns(uint256){
         return address(this).balance;
     }
